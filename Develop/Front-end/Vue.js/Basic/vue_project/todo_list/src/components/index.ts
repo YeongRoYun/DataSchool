9f1d8b65d0633d43e13f38010c735e1b1f38f510
yyr <dbsdudfh01@korea.ac.kr>
2022-07-22 01:45:14 +0900

    .

---
+++ Develop/Front-end/Vue.js/Basic/vue_project/todo_list/src/components/index.ts
@@ -0,0 +1,5 @@
+export {default as TodoListContainer}  from './TodoListContainer.vue'
+export {default as TodoListNew} from './TodoListNew.vue';
+export {default as TodoListMain} from './TodoListMain.vue';
+export {default as TodoListMenu} from './TodoListMenu.vue';
+export {default as TodoList} from './TodoList.vue';
